5eae982a264901d421257e02bc3d3179220b8d93
Murilo Lobo Teixeira <murilolobo92@gmail.com>
2024-05-01 16:48:22 -0400

    lint

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -127,7 +127,14 @@ const SheetDescription = React.forwardRef<
 SheetDescription.displayName = SheetPrimitive.Description.displayName
 
 export {
-  Sheet, SheetClose,
-  SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetOverlay, SheetPortal, SheetTitle, SheetTrigger
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetOverlay,
+  SheetPortal,
+  SheetTitle,
+  SheetTrigger,
 }
-
