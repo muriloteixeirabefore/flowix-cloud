79ecd9525dbc21c503c61e549f131d1aea9caaa6
Murilo Lobo Teixeira <murilolobo92@gmail.com>
2024-05-19 11:13:09 -0400

    wip

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -76,5 +76,5 @@ const nextAuthOptions: NextAuthOptions = {
 
 const handler = NextAuth(nextAuthOptions)
 
-export { handler as GET, handler as POST, nextAuthOptions }
+export { handler as GET, handler as POST }
 
