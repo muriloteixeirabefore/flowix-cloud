40a04c0c1da5f8a1c8b38932d36e62365aa0f78e
Mateus Moreira <mateusgabimoreira@gmail.com>
2024-06-04 01:00:43 -0300

    feat: Add webhook screen

---
+++ app/monitor/webhookCalls/page.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { getWebhookCalls } from '@/app/actions/getWebhookCalls'
+import { H4 } from '@/components/ui/h4'
+import { useQuery } from '@tanstack/react-query'
+import { WebhookTable } from './Webhook'
+
+export default function WebhookCallsPage() {
+  const { data, error } = useQuery({
+    queryKey: ['responses'],
+    queryFn: () => getWebhookCalls(),
+    initialData: [],
+  })
+
+  return (
+    <>
+      <H4>Webhooks</H4>
+      <WebhookTable data={data} />
+    </>
+  )
+}
